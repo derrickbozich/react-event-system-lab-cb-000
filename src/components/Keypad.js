a8157e7423c836ed82dd0428ded7ee8ddf7d6077
derrick b <dbozich1@gmail.com>
2018-10-24 01:43:18 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+// your ImageSlider code here!
+import React from 'react'
+
+export default class Keypad extends React.Component{
+
+  password = () => console.log('Entering password...')
+
+  render(){
+    return(
+      <input type="password" onkeyUp={this.password} />
+    )
+  }
+}
